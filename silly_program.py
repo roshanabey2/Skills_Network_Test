9d502594191ee6dc220852ec605fde8930914511
Roshan Abeysekera <roshanabey2@gmail.com>
2024-06-18 18:11:12 +0100

    Revert "added some code to silly_program.py"  reverts commit be8f875d7764a8096920052436130855f0cdff26.

--- silly_program.py
+++ silly_program.py
@@ -1,15 +1,4 @@
 for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
-
-
-for i in range(1, 101):
     if i % 4 == 0 and i % 6 == 0:
         print("FooBar")
     elif i % 4 == 0:
@@ -18,3 +7,4 @@ for i in range(1, 101):
         print("Bar")
     else:
         print(i)
+
