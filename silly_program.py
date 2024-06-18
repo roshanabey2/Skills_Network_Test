1f0125da49f2c61bc21b32941b13b3441caf69f8
Roshan Abeysekera <roshanabey2@gmail.com>
2024-06-18 18:25:40 +0100

    Added Foobar to silly_program again

--- silly_program.py
+++ silly_program.py
@@ -8,3 +8,12 @@ for i in range(1, 101):
     else:
         print(i)
 
+for i in range(1, 101):
+    if i % 4 == 0 and i % 6 == 0:
+        print("FooBar")
+    elif i % 4 == 0:
+        print("Foo")
+    elif i % 6 == 0:
+        print("Bar")
+    else:
+        print(i)
